4eb13499ccd88d694c2c56a37e0e31d09fbeffa9
alexkali23 <50529116+alexkali23@users.noreply.github.com>
2019-09-22 22:32:08 +0300

    Update settings.py

--- dotadream/dotadream/settings.py
+++ dotadream/dotadream/settings.py
@@ -124,11 +124,9 @@ TEMPLATE_DIRS = (
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT= '/home/username/projects/site/assets/'
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")
 
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
